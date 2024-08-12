2fd3eeb61c37e0b1fa4245c72f87d42856e76512
Luis <luchobava93@gmail.com>
2024-08-12 12:07:20 -0500

    first comit

---
+++ src/components/Tarea.tsx
@@ -0,0 +1,15 @@
+type Tarea ={
+    tarea: string
+    borrarTarea:()=>void
+    
+    }
+    
+    export const Tarea =({tarea, borrarTarea}: Tarea) =>{
+        return(
+            <div className="task">
+                <span>{tarea}</span>
+                <button onClick={borrarTarea}>Borrar</button>
+            
+            </div>
+        )
+    }
